4387086bf565488f8fcae24bbb00b543e3d0035d
GiorgioCappello <giocap1030@gmail.com>
2023-05-12 11:32:17 +0200

    modifica commento

--- recap_1.py
+++ recap_1.py
@@ -5,7 +5,7 @@ dizionario = {}                                 #creo dizionario vuoto
 stringa = input("Inserisci una stringa: ")      #chiedo di inserire una stringa
 for lettera in stringa:                         #scorro le lettere
     if lettera in dizionario:                   
-        dizionario[lettera] += 1                #se è presente aumento il conto
+        dizionario[lettera] += 1                #se la lettera è presente nel dizionario aumento il conto
     else:                                       
         dizionario[lettera] = 1                 #altrimenti la inserisco
 
