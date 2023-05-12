3673e271ff7fb8e68f3339fdc6cca94349b6a5ab
GiorgioCappello <giocap1030@gmail.com>
2023-05-12 11:34:31 +0200

    Esercizio dizionario

--- recap_1.py
+++ recap_1.py
@@ -1,9 +1,8 @@
 #Scrivi un programma che richieda all'utente di inserire una stringa e che conti quante volte ogni lettera appare nella stringa. 
 
-
-dizionario = {}                                 #creo dizionario vuoto
-stringa = input("Inserisci una stringa: ")      #chiedo di inserire una stringa
-for lettera in stringa:                         #scorro le lettere
+dizionario = {}                                 
+stringa = input("Inserisci una stringa: ")      
+for lettera in stringa:                         #scorro le lettere della sringa
     if lettera in dizionario:                   
         dizionario[lettera] += 1                #se la lettera è presente nel dizionario aumento il conto
     else:                                       
