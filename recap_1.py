4a7252dd54a0eb7dbd67fd369467867c7ccfadbb
GiorgioCappello <giocap1030@gmail.com>
2023-05-12 12:46:29 +0200

    Esercizio tre

--- recap_1.py
+++ recap_1.py
@@ -1,6 +1,6 @@
-# 1.Scrivi un programma che richieda all'utente di inserire una stringa e che conti quante volte ogni lettera appare nella stringa. 
+# 1) Scrivi un programma che richieda all'utente di inserire una stringa e che conti quante volte ogni lettera appare nella stringa. 
 
-dizionario = {}                                 
+'''dizionario = {}                                 
 stringa = input("Inserisci una stringa: ") 
 for lettera in stringa:                         # scorro le lettere della sringa
     if lettera in dizionario:                   
@@ -8,11 +8,11 @@ for lettera in stringa:                         # scorro le lettere della sringa
     else:                                       
         dizionario[lettera] = 1                 # altrimenti la inserisco
 
-print("Risultato:", dizionario)
+print("Risultato:", dizionario)'''
 
-# 2.Creare un dizionario che conta le parole in una frase
+# 2) Creare un dizionario che conta le parole in una frase
 
-diz = {}
+'''diz = {}
 frase = input("Inserire frase: ")
 par = frase.split()                     # divido la frase nelle sue parole (senza trattare la punteggiatura)
 
@@ -21,4 +21,14 @@ for parola in par:                      # controllo le parole
         diz[parola] += 1                # se la parola è presente nel dizionario aumento il conto
     else:                                       
         diz[parola] = 1                 # altrimenti la inserisco
-print(diz)
+print(diz)'''
+
+# 3) Scrivere un programma che prenda una lista di numeri come input e rimuove i duplicati dalla lista, lasciando solo i valori "unici". L'ordine degli elementi nella lista deve rimanere invariato
+
+l = input("Inserire lista di numeri separati dallo spazio: ")   # richiedo input
+par = l.split()                                                 # divido l'input 
+lista = []
+for numeri in par:                                              # scorro tutti i numeri
+    if numeri not in lista:                                     
+        lista.append(numeri)                                    # se non è il lista lo aggiungo
+print(lista)
